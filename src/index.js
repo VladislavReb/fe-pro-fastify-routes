e92973f32b291f2b034f48214d9eb4207ba0aeae
VladislavReb <104519014+VladislavReb@users.noreply.github.com>
2022-08-25 16:52:08 +0200

    done home work

--- src/index.js
+++ src/index.js
@@ -11,4 +11,39 @@ fastify.register(import('@fastify/multipart'), {
 });
 fastify.register(import('@fastify/cookie'));
 
+fastify.post('/uppercase', (request, reply) => {
+  const uppCase = request.body.toUpperCase();
+
+  if (uppCase.includes('FUCK')) {
+    return reply.status(403).send('unresolved');
+  }
+  return reply.send(uppCase);
+});
+
+fastify.post('/lowercase', (request, reply) => {
+  const lowCase = request.body.toLowerCase();
+
+  if (lowCase.includes('fuck')) {
+    return reply.status(403).send('unresolved');
+  }
+  return reply.send(lowCase);
+});
+
+fastify.get('/user/:id', (request, reply) => {
+  const {id} = request.params;
+
+  if (!users[id]) {
+    return reply.status(400).send('user not exist');
+  }
+  return reply.send(users[id]);
+});
+
+fastify.get('/users', (request, reply) => {
+  const {filter, value} = request.query;
+  if (!filter && !value) {
+    return reply.send(Object.values(users));
+  }
+  return reply.send(Object.values(users).filter((user) => user[filter].toString() === value));
+});
+
 export default fastify;
